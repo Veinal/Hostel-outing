2b4fb4f8e868c37488e8cd43161a64fef5b631de
Veinal <veinalsequeira5229@gmail.com>
2025-04-28 14:32:15 +0530

    first commit

---
+++ src/components/StudentDashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const StudentDashboard = () => {
+  return (
+    <div>
+        Student Dashboard page
+    </div>
+  )
+}
---
+++ src/components/WardenDashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const WardenDashboard = () => {
+  return (
+    <div>
+        Warden Dashboard page
+    </div>
+  )
+}
